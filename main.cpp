e27fa4b1ccee662f32be790998f431621ef10c07
Dirk Arnez <smalldirkalex@gmail.com>
2025-09-18 17:46:02 +0800

    - update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 #include "gammaramp.h"
 
 
@@ -9,24 +10,36 @@
 //   int nCmdShow              // show state
 // )
 
+void print_usage() {
+	std::cout << "ScreenDimmer.exe on / off" << std::endl;
+}
 
-
-int main(int argc, char * argv[])
+int main(int argc, char* argv[])
 {
 	//Example for changing the brightness with CGammaRamp class:
 	//Be aware that this exmaple may not work properly in all
 	//Video cards.
 
+	char* first_arg = argv[1];
+
+    if (first_arg == nullptr) {
+		print_usage();
+        return EXIT_FAILURE;
+    }
+	
 	CGammaRamp GammaRamp;
-	std::string command = std::string(argv[1]);
+	std::string command = std::string(first_arg);
 	
 	if (command == "on") {
 		//Make the screen darker:
-		GammaRamp.SetBrightness(NULL, 10);
+		GammaRamp.SetBrightness(NULL, 5);
 	} else if (command == "off") {
 		//Return back to normal:
 		GammaRamp.SetBrightness(NULL, 128);
+	} else {
+		print_usage();
+        return EXIT_FAILURE;
 	}
-
-	return 0;
+	
+	return EXIT_SUCCESS;
 }
